ce1085c540f893860cf76cec8fcc0614d1e079b0
Vinicius Souza <vinicius.sfti@gmail.com>
2018-05-26 22:30:15 -0300

    Creating instantiation operation.

--- src/main/java/br/com/cursospring/CursospringApplication.java
+++ src/main/java/br/com/cursospring/CursospringApplication.java
@@ -1,12 +1,31 @@
 package br.com.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringApplication {
+import br.com.cursospring.domain.Category;
+import br.com.cursospring.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursospringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category catOne = new Category(null, "Info");
+		Category catTwo = new Category(null, "Ofiice");
+		
+		categoryRepository.saveAll(Arrays.asList(catOne, catTwo));
+
 	}
 }
